8a6f252d8656e9dacd1a4902c2a385050abba7b3
Ysabella <ysabelladelacruz2022@gmail.com>
2024-05-06 11:38:43 -0700

    added randomizer

--- frontend/src/pages/Random.jsx
+++ frontend/src/pages/Random.jsx
@@ -1,7 +1,33 @@
+import React, { useState } from 'react';
+import RandomButton from "./components/RandomButton"
+import Place from './components/Place';
+
 function Random() {
+  const [rplace, setPlace] = useState(null)
+  const handleClick = async () => {
+    try {
+      const response = await fetch('/randomizer');
+      const randomPlace = await response.json()
+      setPlace(randomPlace);
+    } catch(error) {
+      console.error('Error fetching data:', error);      
+    }
+  };
+
   return (
-    <div>Random</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center gap-4">
+        <div className="p-4">
+          <RandomButton onClick={handleClick} />
+        </div>
+        {rplace ? (
+          <div className="p-4">
+            <Place place={rplace} />
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );  
 }
 
 export default Random
---
+++ frontend/src/pages/components/RandomButton.jsx
@@ -0,0 +1,8 @@
+function RandomButton({ onClick }) {
+    return (
+      <button onClick={onClick} className=' bg-indigo-300 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded'> Let's Explore!
+      </button>
+    );
+  }
+
+export default RandomButton
