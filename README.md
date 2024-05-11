74616fb9c9ac61527428790a0d4bb31461b56c9c
Ysabella <ysabelladelacruz2022@gmail.com>
2024-05-10 23:43:08 -0700

    code cleanup

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# sweet-scout
+# SweetScout
+
+### Project Description
 An application designed to find your perfect sweet treat.
 
 Project Title: SweetScout
@@ -11,8 +13,24 @@ Functionality:
   Through this application we aim to remedy the universal issue of deciding where to eat. As witnessed through personal experiences, we have both struggled to decide on the types of foods and drinks to get, and have wasted a lot of time trying to determine our treat. The application would randomly generate a specific food/drink, depending on what the user is looking for. The result of this is that the users would save a lot of time deciding on what food/drink to get and would now have a decision made for them. 
 
 Application: 
-  Our application will utilize both web scraping and web development. 
+  Our application will utilize both web APIs and web development. 
 
 Approach:
-  To collect the data for our application, will use a combination of the Google Maps API and the BeautifulSoup Python package. We will first querey the Google Maps API for the food locations within a user's vicinity. We will then scrape menu data from location websites to provide a more filtered search if a user choses to provide keywords. To deal with the server-side logic, we will either use the FastAPI package to provide multiple pages. For the frontend of our application we will utilize React to build an interactive user-interface.
+  To collect the data for our application, will use the Google Maps API. We will first querey the Google Maps API for the food locations within a user's vicinity. We will also enable users to keep track of their favorite orders by entering the order name, price, and description. The final feature will include a randomizer to give the user a suggestion to explore. To deal with the server-side logic, we will use the Flask package to provide multiple pages. For the frontend of our application we will utilize React to build an interactive user-interface.
+
+### Running the Application
+Starting from the root directory, run the following command to install packages used to run our backend and frontend concurrently.
+```
+npm i
+```
+Now, cd into the frontend directory to install frontend dependencies.
+```
+cd ../frontend
+npm i
+```
+Finally, cd back into the root directory and run the application.
+```
+cd ..
+npm run dev
+```
   
--- backend/server.py
+++ backend/server.py
@@ -1,34 +1,27 @@
 from flask import Flask, request
+import flask_login
 import requests
 from dotenv import load_dotenv
 import os
 import random 
+import sqlite3
 
-from helper import get_user_location, parse_data
+import helper
 
 app = Flask(__name__)
 orders = []
 
+con = sqlite3.connect("sweetscout.db")
+cur = con.cursor()
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
-location = get_user_location()
+location = helper.get_user_location()
 lat = location['lat']
 lng = location['lng']
 
-@app.route('/')
-def find_nearby_restaurants():
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?&keyword=boba&location={lat},{lng}&radius=5000&&key={API_KEY}"
-    try:
-        response = requests.post(url)
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        else:
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None, None
-    
 @app.route('/sweets')
 def find_nearby_sweets():
     keywords = 'coffee|boba|ice cream|cake|cookies'
@@ -37,13 +30,13 @@ def find_nearby_sweets():
     try:
         response = requests.get(url)
         data = response.json()
-        places = parse_data(data)
+        places = helper.parse_data(data)
         next_page_token = data.get('next_page_token')
         while next_page_token:
             url_with_token = f"{url}&pagetoken={next_page_token}"
             response = requests.get(url_with_token)
             data = response.json()
-            places = places + parse_data(data)
+            places = places + helper.parse_data(data)
             next_page_token = data.get('next_page_token')
 
         for p in places: 
@@ -93,13 +86,13 @@ def get_random_sweet():
     try:
         response = requests.get(url)
         data = response.json()
-        places = parse_data(data)
+        places = helper.parse_data(data)
         next_page_token = data.get('next_page_token')
         while next_page_token:
             url_with_token = f"{url}&pagetoken={next_page_token}"
             response = requests.get(url_with_token)
             data = response.json()
-            places = places + parse_data(data)
+            places = places + helper.parse_data(data)
             next_page_token = data.get('next_page_token')
 
         r_place = random.choice(places)
